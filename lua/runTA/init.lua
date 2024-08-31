b1de4a682cdf2241a524f24ee2572fbd24473f77
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-31 20:10:42 +0530

    init

--- lua/runTA/init.lua
+++ lua/runTA/init.lua
@@ -3,13 +3,12 @@ local runTA = require("runTA.commands")
 local function setup_runTA()
 	runTA.setup({
 		output_window_configs = {
-			width = 80, -- Default width of the floating window
-			height = 20, -- Default height of the floating window
-			position = "center", -- Default position (can be "center", "top", "bottom", "left", "right", "custom")
-			custom_col = nil, -- Column position for custom placement (optional)
-			custom_row = nil, -- Row position for custom placement (optional)
-
-			transparent = false, -- true for a transparent background
+			width = 80,
+			height = 20,
+			position = "center",
+			custom_col = nil,
+			custom_row = nil,
+			transparent = false,
 		},
 	})
 end
