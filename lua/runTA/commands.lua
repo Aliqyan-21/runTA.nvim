add0052247b33bcb25355be3253ee5c09751e168
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-31 16:40:57 +0530

    small bug

--- lua/runTA/commands.lua
+++ lua/runTA/commands.lua
@@ -98,7 +98,7 @@ local function run_code()
 		return
 	end
 
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Code Output:\n" })
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Code Output:" })
 
 	vim.fn.jobstart(command, {
 		on_stdout = function(_, data, _)
