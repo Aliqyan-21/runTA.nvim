3a16977e22813ea4f27c672a9760aed6423cf2f7
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-31 18:14:55 +0530

    init

--- lua/runTA/init.lua
+++ lua/runTA/init.lua
@@ -1,17 +1,21 @@
-local runTA = require("runTA.commands")
+local commands = require("runTA.commands")
+local runner = require("runTA.runner")
 
 local function setup_runTA()
-	runTA.setup({
+	commands.setup({
 		output_window_configs = {
-			width = 80, -- Default width of the floating window
-			height = 20, -- Default height of the floating window
-			position = "center", -- Default position (can be "center", "top", "bottom", "left", "right", "custom")
-			custom_col = nil, -- Column position for custom placement (optional)
-			custom_row = nil, -- Row position for custom placement (optional)
-
-			transparent = false, -- true for a transparent background
+			width = 80,
+			height = 20,
+			position = "center",
+			custom_col = nil,
+			custom_row = nil,
+			transparent = false,
 		},
 	})
+
+	vim.api.nvim_create_user_command("RunCode", function()
+		runner.run_code()
+	end, {})
 end
 
 setup_runTA()
