6ab801809d145e28eedda183fde1f7a0f8c7c5d0
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-31 15:55:03 +0530

    configs in init.lua

--- lua/runTA/init.lua
+++ lua/runTA/init.lua
@@ -1,7 +1,14 @@
 local runTA = require("runTA.commands")
 
-function setup_runTA()
-	runTA.setup()
+local function setup_runTA()
+	-- configurations
+	runTA.setup({
+		width = 80,
+		height = 20,
+		position = "center", -- "center", "top", "bottom", "left", "right", "custom"
+		custom_col = nil, -- Column position for custom placement (optional)
+		custom_row = nil, -- Row position for custom placement (optional)
+	})
 end
 
 setup_runTA()
